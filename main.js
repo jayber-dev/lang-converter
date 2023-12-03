7128159262138b2e58f2f93c2e1c6b74602a040c
jayber <jayber1@gmail.com>
2023-12-03 16:25:29 +0200

    working windows app

--- main.js
+++ main.js
@@ -7,23 +7,27 @@ const { hebKeys } = require('./keys-layout/hebKeys')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 600,
-    height: 600,
+    height: 650,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    icon: "./favicon.ico",
+    backgroundColor: "smokewhite"
   })
 
   win.loadFile('index.html')
+  return win
 }
 
 app.whenReady().then(() => {
-  createWindow()
-
-  const ret = globalShortcut.register('CommandOrControl+l', () => {
+  const win = createWindow()
+  
+  const ret = globalShortcut.register('CommandOrControl+f', () => {
+    win.setIcon("./favicon-processing.ico")
     const text = clipboard.readText()
     let tempText
     let langFlag = ""
-    console.log("rawText: ",text);
+    // console.log("rawText: ",text);
     const splitText = text.split(" ")[0]
     for (let i = 0; i < splitText.length; i++) {
       try {
@@ -35,22 +39,23 @@ app.whenReady().then(() => {
           langFlag = "heb"
         }
       } catch (error) {
-        console.log(error);
+        // console.log(error);
       }
     }
 
     if (langFlag === "eng") {
+      win.icon = "./favicon3.ico"
       for (let i = 0; i < text.length; i++) {
         tempText += (engKeys[text[i]])
       }
     }
 
     if (langFlag === "heb") {
-      console.log(tempText);
-      console.log(hebKeys["."])
+      // console.log(tempText);
+      // console.log(hebKeys["."])
       for (let i = 0; i < text.length; i++) {
 
-        console.log(hebKeys[`${text[i]}`]);
+        // console.log(hebKeys[`${text[i]}`]);
         tempText += (hebKeys[`${text[i]}`])
 
       }
@@ -62,10 +67,11 @@ app.whenReady().then(() => {
 
     const translatedText = (tempText?.split('d')[2])
     clipboard.writeText(translatedText)
+    win.setIcon("./favicon.ico")
   })
 
   app.on('browser-window-blur', () => {
-    console.log("it works")
+    // console.log("out of focus")
   })
 
 })
